cd99ee76d5d51bc9b771d21fbfba40d8bd69dbf7
Filip Lobpreis <f.lobpreis@gmail.com>
2023-08-24 20:53:30 +0200

    feat: use mksession
    Use mksession to save and restore the opened files.
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/revolver/init.lua
+++ lua/revolver/init.lua
@@ -13,15 +13,10 @@ M.project = function()
 end
 
 --- Creates file and the required directories.
----
----@param mode string In which the file should be opened.
----@return file*? Opened file
-local function createFile(mode)
+local function createFile()
 	if vim.fn.isdirectory(saveDir) then
 		os.execute("mkdir -p " .. saveDir)
 	end
-	vim.notify("Creating file " .. saveDir .. M.project(), vim.log.levels.INFO)
-	return io.open(saveDir .. M.project(), mode)
 end
 
 --- Writes data to a file only if the file exists.
@@ -43,34 +38,17 @@ local function readLine(file)
 	if file and vim.fn.filereadable(saveDir) then
 		return file:read("l")
 	else
-		vim.notify("Could not read from the " .. saveDir .. " file", vim.log.levels.ERROR)
+		error("Could not read from the " .. saveDir .. " file")
 		return nil
 	end
 end
 
 --- Saves opened files to a file. Files are representations of projects.
 -- If two projects have the same name of the last directory. The newer save overwrites the older one.
----@return table string Table of strings of opened files
 M.SaveOpenedFiles = function ()
 	local blist = vim.fn.getbufinfo({ buflisted = 1, bufloaded = 1 })
-	local file = createFile("w")
-	if not file then
-		vim.notify("Could not create file " .. saveDir .. M.project(),vim.log.levels.ERROR)
-	else
-		vim.notify("File created " .. saveDir .. M.project(), vim.log.levels.INFO)
-	end
-	local result = {}
-
-	for k,item in ipairs(blist) do
-		if item.name then
-			writeFile(file, item.name)
-			result[k] = item.name
-		end
-	end
-	if file ~= nil then
-		file:close()
-	end
-	return result
+	createFile()
+	vim.cmd("mksession! " .. saveDir .. M.project())
 end
 
 --- Opens the files from the saved file.
@@ -79,22 +57,12 @@ end
 M.OpenSavedFiles = function (deleteAfterLoad)
 	local file = io.open(saveDir .. M.project(), "r")
 	if not file then
-		vim.notify("Could not open file " .. saveDir .. M.project(), vim.log.levels.ERROR)
+		error("Could not open file " .. saveDir .. M.project())
 		return false
 	end
-	local files = 0
-	while true do
-		local line = readLine(file)
-		if not line then
-			break
-		end
-		vim.cmd("e " .. line)
-		files = files + 1
-	end
 	file:close()
-	if deleteAfterLoad then
-		vim.fn.delete(saveDir .. M.project())
-	end
+
+	vim.cmd("source " .. saveDir .. M.project())
 	return true
 end
 
